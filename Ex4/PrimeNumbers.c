a01378d449a379564b4c0c306b5afc7f4a2c50b5
durobor <peterallemann333@gmail.com>
2016-03-16 19:00:08 +0100

    working on an issue with structs

--- Ex4/PrimeNumbers.c
+++ Ex4/PrimeNumbers.c
@@ -1,50 +1,87 @@
 #include <stdio.h>
 #include <math.h>
 
-struct prime {
+typedef struct prime {
 	int p;
 	struct prime *next;
-};
+} prime;
 
 int main(){
 	int limit=0;
 	int candidate;
-	struct prime smalestPrime;
-	struct prime *currentPrime, *biggestPrime;
+	prime smallestPrime;
+	prime *currentPrime, *biggestPrime;
 
 	// Get upper limit from user
 	while(1>=limit){
 		printf("Up to which number would you like to know all the prime numbers? ");
 		scanf("%d", &limit);
 		if(1>=limit){
-			printf("Don't be silly...");
+			printf("Don't be silly...\n");
 		}
 	}
 
 	// Set up variables
-	smalestPrime.p=2;
-	currentPrime=&smalestPrime;
-	biggestPrime=&smalestPrime;
+	smallestPrime.p=2;
+	currentPrime=&smallestPrime;
+	biggestPrime=&smallestPrime;
 	candidate=3;
 
 	// Search for primes below limit
-	printf("%d\n", smalestPrime.p);
+	printf("{%d, ", smallestPrime.p);
 	while(limit>=candidate){
-		if(0==candidate%currentPrime->p){
+//		printf("\nsmallestPrime.p: %d\n", smallestPrime.p);
+//		printf("currentPrime->p: %d\n", currentPrime->p);
+//		printf("biggestPrime->p: %d\n", biggestPrime->p);
+//		printf("sqrt(%d)=%f\n", candidate, sqrt(candidate));
+//		printf("%d%%%d=%d\n", candidate, currentPrime->p, candidate%currentPrime->p);
+		
+		if(0==candidate%(currentPrime->p)){
 			candidate+=2;
-			currentPrime=&smalestPrime;
+			currentPrime=&smallestPrime;
 		}
 		else if(0!=candidate%currentPrime->p && sqrt(candidate)>=currentPrime->p){
 			currentPrime=currentPrime->next;
 		}
 		else{
-			struct prime newPrimeNmbr;
-			newPrimeNmbr.p=candidate;
-			biggestPrime->next=&newPrimeNmbr;
-			biggestPrime=biggestPrime->next;
-			currentPrime=&smalestPrime;
+			//debugging===============================================================
+//			if(5==candidate){
+//				printf("candidate: %d", candidate);
+//				printf("\nsmallestPrime.p: %d\n", smallestPrime.p);
+//				printf("smallestPrime.next->p: %d\n", smallestPrime.next->p);
+//				printf("currentPrime->p: %d\n", currentPrime->p);
+//				printf("currentPrime->next->p: %d\n", currentPrime->next->p);
+//				printf("biggestPrime->p: %d\n", biggestPrime->p);
+//			}
+			//debugging===============================================================
+			//smallestPrime: 2
+			//currentPrime: 3
+			//biggestPrime: 3
+				if(5==candidate) printf("\nsmallestPrime.next->p: %d\n", smallestPrime.next->p);
+			prime newPrime = { candidate };
+//			newPrime.p=candidate;
+				if(5==candidate) printf("\nsmallestPrime.next->p: %d\n", smallestPrime.next->p);
+			biggestPrime->next=&newPrime;
+			biggestPrime=&newPrime;
+			currentPrime=&smallestPrime;
+			//newPrime: 5
+			//biggest->next: 5
+			//biggest: 5
+			/*
+			//debugging===============================================================
+			if(5==candidate){
+				printf("candidate: %d", candidate);
+				printf("\nsmallestPrime.p: %d\n", smallestPrime.p);
+				printf("smallestPrime->next->p: %d\n", smallestPrime.next->p);
+				printf("currentPrime->p: %d\n", currentPrime->p);
+				printf("currentPrime->next->p: %d\n", currentPrime->next->p);
+				printf("biggestPrime->p: %d\n", biggestPrime->p);
+			}
+			//debugging===============================================================
+			//*/
 			candidate+=2;
-			printf("%d\n", biggestPrime->p);
+			printf("%d, ", biggestPrime->p);
 		}
 	}
+	printf("}\n");
 }
