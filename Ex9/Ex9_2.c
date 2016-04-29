70dc346eed8a16286af5a8fecd5bdcc2c1e74dab
durobor <peterallemann333@gmail.com>
2016-04-29 13:51:14 +0200

    Everything seems to be working.

--- Ex9/Ex9_2.c
+++ Ex9/Ex9_2.c
@@ -11,9 +11,11 @@
 #define RIGHT 5.f
 
 double func_A(double);
+double func_A_der(double);
 void printRootsBisectionMethod();
-double findSingleRootBisectionMethod(double,double);
 void printRootsNewtonRaphsonMethod();
+double findSingleRootBisectionMethod(double,double);
+double findSingleRootNewtonMethod(double);
 
 int main(){
 	printRootsBisectionMethod();
@@ -26,6 +28,11 @@ double func_A(double t){
 	return A0*pow(M_E, -GAMMA*t)*(cos(OMEGA1*t)+sin(OMEGA2*t));
 }
 
+double func_A_der(double t){
+	return -A0*GAMMA*pow(M_E, -GAMMA*t)*(cos(OMEGA1*t)+sin(OMEGA2*t))
+		+A0*pow(M_E, -GAMMA*t)*(-OMEGA1*sin(OMEGA1*t)+OMEGA2*cos(OMEGA2*t));
+}
+
 void printRootsBisectionMethod(){
 	printf("Roots found by Bisection Method:\n");
 	printf("%fl\n", findSingleRootBisectionMethod(0.5L, 1.5L));
@@ -34,6 +41,16 @@ void printRootsBisectionMethod(){
 	printf("%fl\n", findSingleRootBisectionMethod(2.5L, 3.5L));
 	printf("%fl\n", findSingleRootBisectionMethod(3.5L, 4.05L));
 	printf("%fl\n", findSingleRootBisectionMethod(4.05L, 4.5L));
+}
+
+void printRootsNewtonRaphsonMethod(){
+	printf("Roots found by Newton-Raphson Method:\n");
+	printf("%fl\n", findSingleRootNewtonMethod(0.5L));
+	printf("%fl\n", findSingleRootNewtonMethod(1.5L));
+	printf("%fl\n", findSingleRootNewtonMethod(1.9L));
+	printf("%fl\n", findSingleRootNewtonMethod(2.8L));
+	printf("%fl\n", findSingleRootNewtonMethod(3.8L));
+	printf("%fl\n", findSingleRootNewtonMethod(4.5L));
 }
 
 double findSingleRootBisectionMethod(double left,double right){
@@ -51,5 +68,12 @@ double findSingleRootBisectionMethod(double left,double right){
 	return c;
 }
 
-void printRootsNewtonRaphsonMethod(){
+double findSingleRootNewtonMethod(double str_pnt){
+	double epsilon=1.l, a=str_pnt, b;
+	while((PRECISION<epsilon && 0<epsilon) || (PRECISION<(0-epsilon) && 0>epsilon)){
+		b=a-func_A(a)/func_A_der(a);
+		epsilon=b-a;
+		a=b;
+	}
+	return a;
 }
