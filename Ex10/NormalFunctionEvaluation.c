1c2593322c92fdbc27e0de49591b99cb19fb5a83
durobor <peterallemann333@gmail.com>
2016-05-07 13:13:52 +0200

    Simpsons method works, but implementation is not 100% correct. It is only half as fast as it could be. Correct implementation can be found on wikipedia.

--- Ex10/NormalFunctionEvaluation.c
+++ Ex10/NormalFunctionEvaluation.c
@@ -5,6 +5,7 @@
 
 double f(double);
 double integrate_Trapezoidal(double (*func)(double),double,double,double);
+double integrate_Simpson(double (*func)(double),double,double,double);
 
 int main(){
 	int i;
@@ -13,12 +14,10 @@ int main(){
 		printf("%d\t%.8fl\n", i, integrate_Trapezoidal(f,-i,i,PRECISION));
 	}
 
-	/*
 	printf("Simpson method:\nt\tValue(t)\n");
 	for(i=1;i<=5;++i){
-		printf("%d\t%.8fl\n", i, integrate_Trapezoidal(f,-i,i,PRECISION));
+		printf("%d\t%.8fl\n", i, integrate_Simpson(f,-i,i,PRECISION));
 	}
-	*/
 	
 	return 0;
 }
@@ -38,7 +37,25 @@ double integrate_Trapezoidal(double (*func)(double), double left, double right, 
 		integral_1=c/n*j_1;
 		n*=2;
 	}while(fabs(integral_1-integral_2)>precision*fabs(integral_2));
-//	printf("%.0fl\n", log2(n));
-//	printf("%.0fl\n", n);
+//	printf("Iterations: %.0fl\n", log2(n));
+//	printf("Number of function calls: %.0fl\n", n);
 	return integral_1;
+}
+
+double integrate_Simpson(double (*func)(double), double left, double right, double precision){
+	double n=1.l, c=(right-left), h, integral=func(left)+func(right);
+	int i;
+	do{
+		n*=2;
+		for(i=1;i<n;i+=2){
+			integral+=2*func(left+i/n*c);
+		}
+	}while(precision<1/pow(n,4));
+	for(i=1;i<n;i+=2){
+			integral+=2*func(left+i/n*c);
+	}
+	h=c/n;
+//	printf("Iterations: %.0fl\n", log2(n));
+//	printf("Number of function calls: %.0fl\n", n);
+	return h/3.l*integral;
 }
