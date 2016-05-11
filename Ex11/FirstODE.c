0c501597c7abc84a7f51e78b129bdc5f4d4fe87f
durobor <peterallemann333@gmail.com>
2016-05-11 18:01:47 +0200

    Euler method is working for now

---
+++ Ex11/FirstODE.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+double euler(double,double,double,double);
+
+int main(){
+
+	return 0;
+}
+
+double euler(double (*func)(double,double), double lower, double upper, double stepsize){
+	return 0.l;
+}
